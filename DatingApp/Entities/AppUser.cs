d2d4bdceacd10626c689d1ad4edf61ff1c6bc213
Bui Dung <dungbuivm@gmail.com>
2023-08-16 12:02:29 +0700

    8.2 Extending the user entity 8.3 Adding a DateTime extension to calculate age

--- DatingApp/Entities/AppUser.cs
+++ DatingApp/Entities/AppUser.cs
@@ -1,4 +1,9 @@
-﻿namespace DatingApp.Entities
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DatingApp.Extensions;
+
+namespace DatingApp.Entities
 {
     public class AppUser
     {
@@ -8,5 +13,18 @@
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public ICollection<Photo> MyProperty { get; set; }
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge(); 
+        }
     }
 }
