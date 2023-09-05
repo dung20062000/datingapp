69fc595fd160f07dfcf24bcb4af2089f07c81126
Bui Dung <dungbuivm@gmail.com>
2023-09-05 11:15:36 +0700

    16.10 Adding policy based authorisation

---
+++ DatingApp/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    public class AdminController: BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public ActionResult GetUsersWithRoles()
+        {
+            return Ok("Only admins cam see this");
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admin or moderate cam see this");
+        }
+    }
+}
